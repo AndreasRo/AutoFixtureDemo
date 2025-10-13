b72e9a50857536d494638231e79af79df92fc2eb
Andreas Rodtwitt <andreas.rodtwitt@gmail.com>
2025-10-13 10:38:09 +0200

    Swagger

--- AutoFixtureDemo/Program.cs
+++ AutoFixtureDemo/Program.cs
@@ -5,8 +5,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddSingleton<IWeatherForecastService, WeatherForecastService>(); // Register the service
+// Add Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Enable middleware to serve generated Swagger as a JSON endpoint and the Swagger UI.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
